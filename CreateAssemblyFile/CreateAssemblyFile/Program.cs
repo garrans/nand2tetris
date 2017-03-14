ff4dfc3069e50876d474e8240d9c66278a0a07da
steveg <codechange@garran.com>
2017-03-14 12:31:50 -0600

    File Ops Setup and Structure pseudocode

--- CreateAssemblyFile/CreateAssemblyFile/Program.cs
+++ CreateAssemblyFile/CreateAssemblyFile/Program.cs
@@ -4,19 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Program Organization
+/// 
+/// Class Program
+///     Manages operation of the entire program.... v.v. the procedural aspects
+///     
+/// Class FileOps
+///     File Operations & Methods
+///     means of creating a file handler, opening it etc.
+/// 
+/// Class Assembler
+///     parse the file twice
+///     
+///     
+/// </summary>
+
 namespace CreateAssemblyFile
 {
-    class Program
-    {
+    class program
+   {
+        enum Parser_CommandType { Parser_NO_COMMAND = 0, Parser_A_COMMAND, Parser_C_COMMAND, Parser_L_COMMAND }; 
         static void Main(string[] args)
         {
+            string filename;
+            filename = "";
+            string[] outputstrings = { };
+
+
+            fileOps assemblerfile = new fileOps();
+
             // Get Filename
-            // Create outputfile.hack
-            // create Assembler instancej
+
+            while (filename == "")
+            {
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("No arguments, need to source them");
+                    Console.WriteLine("Please enter the filename of the file you want to read in");
+                    filename = Console.ReadLine();
+                    if (filename.Length==0)
+                    {
+                        Console.WriteLine("No seriously give me a filename!");
+                        filename = Console.ReadLine();
+                    }
+                }
+                else
+                {
+                    filename = args[0];
+                    Console.WriteLine(args[0]);
+                }
+
+            }
+            assemblerfile.readFile(filename, "assembler");
+
+            // create Assembler instance
              
             // do assembler pass1
             // Do assembler pass2
-            // close the files
+            
+            // Create outputfile.hack
+            fileOps hackfile = new fileOps();
+            hackfile.writeFile(filename, "hack", outputstrings);
+
+
+            Console.ReadLine();
+            Console.WriteLine("End of Program, press Enter to End");
+            Console.ReadLine();
+
         }
     }
 }
---
+++ CreateAssemblyFile/CreateAssemblyFile/fileOps.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateAssemblyFile
+{
+    class fileOps
+    {
+        
+        public static void initializeMain()
+        //initialize fileOps
+        {
+            Console.WriteLine("Main Initialize");
+
+        }
+        //open file for read
+        public void writeFile(string stringfileName, string filetype, string[] outputfile)
+        {
+            Console.WriteLine("fileOps.openfile");
+            Console.WriteLine(stringfileName);
+            //for each string in outputfile[]
+                // write string
+            //endfor
+           
+        }
+        
+        public string[] readFile(string stringfileName, string filetype)
+        {
+            string[] filestring = { "hello", "how", "are", "you" };
+            Console.WriteLine("fileOps.openfile");
+            Console.WriteLine(stringfileName);
+            return filestring;
+
+        }
+        
+        //read line
+        //close file
+        //open file for write
+        //write line
+
+
+       //` public static fileOps assemblerfile { get; private set; }
+
+
+    }
+}
